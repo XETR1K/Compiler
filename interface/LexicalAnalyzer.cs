209563389decbb86c9bddeb608403abe9e11f0d9
Alexandr <falogik@gmai.com>
2023-05-06 22:36:03 +0700

    *Изменение лексического анализатора под задание курсовой работы

--- interface/LexicalAnalyzer.cs
+++ interface/LexicalAnalyzer.cs
@@ -6,37 +6,44 @@ public static class LexicalAnalyzer
     // Перечисление для описания типов лексем
     enum TokenType
     {
-        KeywordInt = 1,     // Ключевое слово int
-        KeywordLong,        // Ключевое слово long
-        KeywordShort,       // Ключевое слово short
-        KeywordBoolean,     // Ключевое слово boolean
-        KeywordDouble,      // Ключевое слово double
-        KeywordString,      // Ключевое слово String
-        KeywordTrue,        // Ключевое слово true
-        KeywordFalse,       // Ключевое слово false
-        Identifier,         // Идентификатор
-        SeparatorSpace,     // Разделитель пробел
-        SeparatorNewLine,   // Разделитель \n
-        SeparatorTab,       // Разделитель \t 
-        Assignment,         // Оператор присваивания
-        Semicolon,          // Конец оператора
-        String,             // Строка
-        Integer,            // Целое число
-        Double,             // Вещественное число
-        Invalid             // Недопустимый символ
+        Identifier, //идентификатор
+        IntegerLiteral, //целое число
+        FloatingPointLiteral, //число с плавающей точкой
+        BooleanLiteral, //true и false
+        RelationalOp, //Операторы сравнения
+        AdditiveOp, MultiplicativeOp,//арифметические операторы
+        LogicalOp, //логические операторы
+        Not, //оператор отрицания
+        LeftParen, RightParen, //скобки
+        Invalid
     }
 
-    // Словарь для хранения ключевых слов
+    // Словарь для хранения лексем
     private static readonly Dictionary<string, TokenType> keywords = new Dictionary<string, TokenType>()
     {
-        { "int", TokenType.KeywordInt },
-        { "long", TokenType.KeywordLong },
-        { "short", TokenType.KeywordShort },
-        { "boolean", TokenType.KeywordBoolean },
-        { "double", TokenType.KeywordDouble },
-        { "String", TokenType.KeywordString },
-        { "true", TokenType.KeywordTrue },
-        { "false", TokenType.KeywordFalse }
+        // Ключевые слова
+        { "true", TokenType.BooleanLiteral },
+        { "false", TokenType.BooleanLiteral },
+
+        // Операторы
+        { "!", TokenType.Not },
+        { "&&", TokenType.LogicalOp },
+        { "||", TokenType.LogicalOp },
+        { "<", TokenType.RelationalOp },
+        { ">", TokenType.RelationalOp },
+        { "<=", TokenType.RelationalOp },
+        { ">=", TokenType.RelationalOp },
+        { "==", TokenType.RelationalOp },
+        { "!=", TokenType.RelationalOp },
+        { "+", TokenType.AdditiveOp },
+        { "-", TokenType.AdditiveOp },
+        { "*", TokenType.MultiplicativeOp },
+        { "/", TokenType.MultiplicativeOp },
+        { "%", TokenType.MultiplicativeOp },
+
+        // Скобки
+        { "(", TokenType.LeftParen },
+        { ")", TokenType.RightParen },
     };
 
     // Метод для проверки, является ли символ буквой
@@ -52,146 +59,191 @@ public static class LexicalAnalyzer
     }
 
     // Метод для выделения лексем
-    private static List<Tuple<int, int, TokenType, string, int, int>> Tokenize(string input)
+    private static List<Tuple<TokenType, string, int, int>> Tokenize(string input)
     {
-        List<Tuple<int, int, TokenType, string, int, int>> tokens = new List<Tuple<int, int, TokenType, string, int, int>>(); // Список для хранения лексем
-        int i = 0; // Индекс текущего символа
-        int line = 1; //Номер текущей строки
+        var tokens = new List<Tuple<TokenType, string, int, int>>();
 
-        while (i < input.Length)
+        int position = 0;
+
+        while (position < input.Length)
         {
-            char c = input[i];
+            char currentChar = input[position];
 
-            // Если текущий символ является разделителем, пропускаем его
-            if (char.IsWhiteSpace(c))
+            // Пропускаем пробелы и символы перевода строки
+            if (char.IsWhiteSpace(currentChar))
             {
-                // Если это пробел или перевод строки, добавляем соответствующую лексему
-                if (c == ' ')
-                {
-                    tokens.Add(new Tuple<int, int, TokenType, string, int, int>(line, (int)TokenType.SeparatorSpace, TokenType.SeparatorSpace, "(space)", i + 1, i + 1));
-                }
-                else if (c == '\n')
-                {
-                    tokens.Add(new Tuple<int, int, TokenType, string, int, int>(line, (int)TokenType.SeparatorNewLine, TokenType.SeparatorNewLine, "\\n", i + 1, i + 1));
-                    line++;
-                }
-                else if (c == '\t')
-                {
-                    tokens.Add(new Tuple<int, int, TokenType, string, int, int>(line, (int)TokenType.SeparatorTab, TokenType.SeparatorTab, "\\t", i + 1, i + 1));
-                }
-                i++;
+                position++;
                 continue;
             }
 
-            // Если текущий символ является оператором присваивания (=)
-            if (c == '=')
+            // Идентификаторы и ключевые слова
+            if (IsLetter(currentChar))
             {
-                tokens.Add(new Tuple<int, int, TokenType, string, int, int>(line, (int)TokenType.Assignment, TokenType.Assignment, "=", i + 1, i + 1));
-                i++;
+                int start = position;
+                while (position < input.Length && (IsLetter(input[position]) || IsDigit(input[position])))
+                {
+                    position++;
+                }
+                string identifier = input.Substring(start, position - start);
+                if (keywords.ContainsKey(identifier))
+                    tokens.Add(new Tuple<TokenType, string, int, int>(keywords[identifier], identifier, start, position - 1));
+                else
+                    tokens.Add(new Tuple<TokenType, string, int, int>(TokenType.Identifier, identifier, start, position - 1));
                 continue;
             }
 
-            // Если текущий символ является концом оператора (;)
-            if (c == ';')
+            // Целые и дробные числа
+            if (IsDigit(currentChar) || currentChar == '.')
             {
-                tokens.Add(new Tuple<int, int, TokenType, string, int, int>(line, (int)TokenType.Semicolon, TokenType.Semicolon, ";", i + 1 , i + 1));
-                i++;
-                continue;
-            }
+                int start = position;
+                bool hasDecimalPoint = false;
 
-            // Если текущий символ является кавычкой (") - начало строки
-            if (c == '"')
-            {
-                string str = "\"";
-                int start = i;
-                i++;
-
-                // Пока не найдем закрывающую кавычку или конец строки
-                while (i < input.Length && input[i] != '\n' && input[i] != '"')
+                while (position < input.Length && (IsDigit(input[position]) || (!hasDecimalPoint && input[position] == '.')))
                 {
-                    str += input[i];
-                    i++;
-                }
-
-                // Если строка закончилась на кавычку, то добавляем лексему в список
-                if (i < input.Length && input[i] == '"')
-                {
-                    str += "\"";
-                    tokens.Add(new Tuple<int, int, TokenType, string, int, int>(line, (int)TokenType.String, TokenType.String, str, start + 1, i));
-                    i++;
-                }
-                else // Иначе строка была не закрыта - это ошибка
-                {
-                    tokens.Add(new Tuple<int, int, TokenType, string, int, int>(line, (int)TokenType.Invalid, TokenType.Invalid, str, start + 1, i));
-                }
-
-                continue;
-            }
-
-            // Если текущий символ является цифрой, то это может быть число
-            if (IsDigit(c))
-            {
-                string number = "";
-                int start = i;
-
-                // Пока текущий символ является цифрой или точкой, добавляем его к числу
-                while (i < input.Length && (IsDigit(input[i]) || input[i] == '.'))
-                {
-                    number += input[i];
-                    i++;
-                }
-
-                // Если число закончилось на точку, то это ошибка
-                if (number.EndsWith("."))
-                {
-                    tokens.Add(new Tuple<int, int, TokenType, string, int, int>(line, (int)TokenType.Invalid, TokenType.Invalid, number, start + 1, i));
-                }
-                else // Иначе добавляем лексему в список
-                {
-                    if (number.Contains(".")) // Если число содержит точку, то это вещественное число
+                    if (input[position] == '.')
                     {
-                        tokens.Add(new Tuple<int, int, TokenType, string, int, int>(line, (int)TokenType.Double, TokenType.Double, number, start + 1, i));
+                        hasDecimalPoint = true;
                     }
-                    else // Иначе это целое число
-                    {
-                        tokens.Add(new Tuple<int, int, TokenType, string, int, int>(line, (int)TokenType.Integer, TokenType.Integer, number, start + 1, i));
-                    }
+                    position++;
+                }
+
+                string number = input.Substring(start, position - start);
+
+                if (hasDecimalPoint)
+                {
+                    tokens.Add(new Tuple<TokenType, string, int, int>(TokenType.FloatingPointLiteral, number, start, position - 1));
+                }
+                else
+                {
+                    tokens.Add(new Tuple<TokenType, string, int, int>(TokenType.IntegerLiteral, number, start, position - 1));
                 }
 
                 continue;
             }
 
-            // Если текущий символ является буквой, то это может быть ключевое слово или идентификатор
-            if (IsLetter(c))
+            // Операторы и скобки
+            switch (currentChar)
             {
-                string word = "";
-                int start = i;
+                case '!':
+                    tokens.Add(new Tuple<TokenType, string, int, int>(TokenType.Not, "!", position, position));
+                    position++;
+                    break;
+                case '&':
+                    if (position < input.Length - 1 && input[position + 1] == '&')
+                    {
+                        tokens.Add(new Tuple<TokenType, string, int, int>(TokenType.LogicalOp, "&&", position, position + 1));
+                        position += 2;
+                    }
+                    else
+                    {
+                        tokens.Add(new Tuple<TokenType, string, int, int>(TokenType.Invalid, currentChar.ToString(), position, position));
+                        position++;
+                    }
+                    break;
+                case '|':
+                    if (position < input.Length - 1 && input[position + 1] == '|')
+                    {
+                        tokens.Add(new Tuple<TokenType, string, int, int>(TokenType.LogicalOp, "||", position, position + 1));
+                        position += 2;
+                    }
+                    else
+                    {
+                        tokens.Add(new Tuple<TokenType, string, int, int>(TokenType.Invalid, currentChar.ToString(), position, position));
+                        position++;
+                    }
+                    break;
+                case '<':
+                    if (position < input.Length - 1 && input[position + 1] == '=')
+                    {
+                        // Оператор <=
+                        tokens.Add(new Tuple<TokenType, string, int, int>(TokenType.RelationalOp, "<=", position, position + 1));
+                        position += 2;
+                    }
+                    else
+                    {
+                        // Оператор <
+                        tokens.Add(new Tuple<TokenType, string, int, int>(TokenType.RelationalOp, "<", position, position));
+                        position++;
+                    }
+                    break;
 
-                // Пока текущий символ является буквой или цифрой, добавляем его к слову
-                while (i < input.Length && (IsLetter(input[i]) || IsDigit(input[i])))
-                {
-                    word += input[i];
-                    i++;
-                }
+                case '>':
+                    if (position < input.Length - 1 && input[position + 1] == '=')
+                    {
+                        // Оператор >=
+                        tokens.Add(new Tuple<TokenType, string, int, int>(TokenType.RelationalOp, ">=", position, position + 1));
+                        position += 2;
+                    }
+                    else
+                    {
+                        // Оператор >
+                        tokens.Add(new Tuple<TokenType, string, int, int>(TokenType.RelationalOp, ">", position, position));
+                        position++;
+                    }
+                    break;
+                case '=':
+                    if (position < input.Length - 1 && input[position + 1] == '=')
+                    {
+                        // Оператор ==
+                        tokens.Add(new Tuple<TokenType, string, int, int>(TokenType.RelationalOp, "==", position, position + 1));
+                        position += 2;
+                    }
+                    else
+                    {   //Некорректный символ
+                        tokens.Add(Tuple.Create(TokenType.Invalid, "=", position, position));
+                        position++;
+                    }
+                    break;
 
-                // Если слово является ключевым, то добавляем лексему в список
-                if (keywords.ContainsKey(word))
-                {
-                    tokens.Add(new Tuple<int, int, TokenType, string, int, int>(line, (int)keywords[word], keywords[word], word, start + 1, i));
-                }
-                else // Иначе это идентификатор
-                {
-                    tokens.Add(new Tuple<int, int, TokenType, string, int, int>(line, (int)TokenType.Identifier, TokenType.Identifier, word, start + 1, i));
-                }
+                case '+':
+                    // Оператор +
+                    tokens.Add(new Tuple<TokenType, string, int, int>(TokenType.AdditiveOp, "+", position, position));
+                    position++;
+                    break;
 
-                continue;
+                case '-':
+                    // Оператор -
+                    tokens.Add(new Tuple<TokenType, string, int, int>(TokenType.AdditiveOp, "-", position, position));
+                    position++;
+                    break;
+
+                case '*':
+                    // Оператор *
+                    tokens.Add(new Tuple<TokenType, string, int, int>(TokenType.MultiplicativeOp, "*", position, position));
+                    position++;
+                    break;
+
+                case '/':
+                    // Оператор /
+                    tokens.Add(new Tuple<TokenType, string, int, int>(TokenType.MultiplicativeOp, "/", position, position));
+                    position++;
+                    break;
+
+                case '%':
+                    // Оператор %
+                    tokens.Add(new Tuple<TokenType, string, int, int>(TokenType.MultiplicativeOp, "%", position, position));
+                    position++;
+                    break;
+
+                case '(':
+                    // Открывающая скобка
+                    tokens.Add(new Tuple<TokenType, string, int, int>(TokenType.LeftParen, "(", position, position));
+                    position++;
+                    break;
+
+                case ')':
+                    // Закрывающая скобка
+                    tokens.Add(new Tuple<TokenType, string, int, int>(TokenType.RightParen, ")", position, position));
+                    position++;
+                    break;
+
+                default:
+                    // Некорректный символ
+                    tokens.Add(new Tuple<TokenType, string, int, int>(TokenType.Invalid, input[position].ToString(), position, position));
+                    position++;
+                    break;
             }
-
-            // Недопустимый символ - добавляем лексему в список
-            tokens.Add(new Tuple<int, int, TokenType, string, int, int>(line, (int)TokenType.Invalid, TokenType.Invalid, c.ToString(), i + 1, i + 1));
-            i++;
         }
-
         return tokens;
     }
     //Метод для запуска сканера
@@ -201,7 +253,7 @@ public static class LexicalAnalyzer
         string result = "";
         foreach (var token in tokens)
         {
-            result += $"{token.Item1,-20} код:{token.Item2,-20} {token.Item3, -20} {token.Item4, -20} c {token.Item5} по {token.Item6}\n";
+            result += $"{token.Item1,-20} {token.Item2,-20} с {token.Item3} до {token.Item4}\n";
         }
         return result;
     }
